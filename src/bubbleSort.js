8427a010f881f0e057edc880de7d61eb40299f7a
MariahCasciola417 <117303840+MariahCasciola@users.noreply.github.com>
2023-03-02 11:01:56 -0600

    bubbleSort function passes all tests

--- src/bubbleSort.js
+++ src/bubbleSort.js
@@ -1,5 +1,26 @@
 function bubbleSort(compare, elements) {
-  
+  if (Array.isArray(elements)) {
+    let inOrder;
+
+    do {
+      inOrder = true; //array is in order, until the difference of right and left is zero
+
+      for (
+        let index = 0, length = elements.length - 1;
+        index < length;
+        index++
+      ) {
+        const leftElement = elements[index];
+        const rightElement = elements[index + 1];
+
+        if (compare(leftElement, rightElement) > 0) {
+          elements[index] = rightElement;
+          elements[index + 1] = leftElement;
+          inOrder = false; // The array wasn't in order becuase the differnce is still more than, so swap elements and then check it again.
+        }
+      }
+    } while (inOrder === false);
+  }
   return elements;
 }
 
